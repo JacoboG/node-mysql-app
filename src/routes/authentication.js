3f0bf69ac83cc01f2d888f8c90c34c123dbec9c9
Omar Jacobo García <omar_192222@live.com>
2021-07-13 16:09:57 -0500

    Formulario SignUp

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/signup', (req, res) => {
+    res.render('auth/signup');
+});
+
+router.post('/signup', (req, res) => {
+    const { fullname, username, password } = req.body;
+    res.send('received')
+});
+
 module.exports = router;
